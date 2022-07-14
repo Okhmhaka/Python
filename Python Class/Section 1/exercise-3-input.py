353258c8e2528df8e7540f934f1aeb783d95842b
Okhmhaka <github@conclavemail.com>
2022-07-14 12:28:49 -0400

    Added exercises

---
+++ Python Class/Section 1/exercise-3-input.py
@@ -0,0 +1,5 @@
+# This exercise will require the application to ask the user for a name and the app will count the letters in the name and print to the screen.
+
+name = input("What is your name? ")
+print("Welcome to the app " + name + "!!")
+print(name + ", your name has " + str(len(name)) + " letters in it!")
