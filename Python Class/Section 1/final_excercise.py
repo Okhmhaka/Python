ed08b6ea2ad01263b879c7d8a3bd5bc6cd83722a
Okhmhaka <github@conclavemail.com>
2022-07-14 11:33:38 -0400

    Added new folders

---
+++ Python Class/Section 1/final_excercise.py
@@ -0,0 +1,12 @@
+# Create a greeting for the application:
+print("Welcome to the band generator app!!")
+print("This app will take some inputs from you and generate a cool band name!")
+
+# Ask the user what city they live in
+city = input("What city did you grow up in?\n")
+
+# Ask the user what their pets name is
+pet_name = input("What is your pets name?\n")
+
+# Combine or randomize the list of inputs to generate a name
+print("The name of your band is " + city + " " + pet_name + "!!")
