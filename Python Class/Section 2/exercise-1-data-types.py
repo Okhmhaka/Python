b4f7783818a40c3ecba1faeb4d026c499a58dce3
Okhmhaka <github@conclavemail.com>
2022-07-15 09:23:07 -0400

    New Exercise

---
+++ Python Class/Section 2/exercise-1-data-types.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+
+total_sum = int(first_number) + int(second_number)
+
+#print(str(first_number) + ' + ' + str(second_number) + " = " + str(total_sum))
+print(total_sum)
