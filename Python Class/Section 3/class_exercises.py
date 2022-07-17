266920a26cba2e4f5cf9e9496cc0674ad0eb9085
Okhmhaka <github@conclavemail.com>
2022-07-17 07:30:39 -0400

    New Exercises

--- Python Class/Section 3/class_exercises.py
+++ Python Class/Section 3/class_exercises.py
@@ -1,17 +1,27 @@
 # Code to determine if you are high enough to ride the amusement park ride. s
 
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-age = int(input("What is your age? "))
+height = int(input("What is your height in cm? \n"))
+age = int(input("What is your age? \n"))
 min_height = 120
 if height >= min_height:
-    print("You are tall enough to ride.")
+    print("You are tall enough to ride.\n")
     #age = int(input("What is your age? "))
     if age < 12:
-        print("The cost of admission is $5. ")
+        print("Child Tickets are $5.\n ")
+        ticket_price = 5
     elif age <= 18:
-        print("The cost of admission is $7. ")
+        print("Youth Tickets are $7.\n ")
+        ticket_price = 7
     else:
-        print("The cost of admission is $12")
+        print("Adult Tickets are $12.\n")
+        ticket_price = 12
+    print("Photos are an additional $3 per ticket.\n ")
+    wants_photo = input("Do you want a photo taken? Y or N: \n")
+    if wants_photo == "y":
+        total = ticket_price + 3
+        print(f"The total price of your ticket is {total} ")
+    else:
+        print(f"The total price of your ticket is {ticket_price} ")
 else:
     print("You are not tall enough to ride.")
