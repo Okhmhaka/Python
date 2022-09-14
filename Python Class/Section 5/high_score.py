e053928a6c1627e1ff1174d406d58d556239029f
Okhmhaka <github@conclavemail.com>
2022-09-13 20:53:43 -0400

    new day

---
+++ Python Class/Section 5/high_score.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+print(f"The highest score in the class is: {high_score}")
