084eb79db675c01bd414688ed3bd5c1e965767b1
Okhmhaka <github@conclavemail.com>
2022-09-21 19:34:25 -0400

    password

--- Python Class/Section 5/secure_password.py
+++ Python Class/Section 5/secure_password.py
@@ -1,2 +1,12 @@
 # This will be a password generator that will provide a secure password.
 
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = lower.upper()
+numbers = "1234567890"
+symbols = "!@#$%^&*()"
+
+length = input("How long do you wnat your password to be?  ")
+
+
