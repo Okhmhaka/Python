cd2962b237b3c783dd8a16bc20bb38254c3683d6
Okhmhaka <github@conclavemail.com>
2022-10-03 08:36:14 -0400

    whileloops

---
+++ Python Class/Section 6/whileloop.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move
+
+
+number_of_hurdles = 5
+
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
