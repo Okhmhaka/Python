b24f4d4cee0ed818fc6a32deddd7ee09d1427793
Okhmhaka <github@conclavemail.com>
2022-12-29 09:00:03 -0500

    hangman2 updates

--- Python Class/Section 7/hangman2.py
+++ Python Class/Section 7/hangman2.py
@@ -1,23 +1,36 @@
 # Step 2
-
+# Import functions and set variables:
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+word_length = len(chosen_word)
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
+# Create an empty list called display for the chosen_word
 display = []
-word_length = len(chosen_word)
+
+# A for loop which puts a "_" in each position of the list for the chosen_word
 for _ in range(word_length):
     display += "_"
-print(display)
 
-guess = input("Guess a letter: ").lower()
+# Insert a while loop so the user can keep guessing. The while loop should stop once all letters have been guessed.
+end_of_game = False
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+#guess = input("Guess a letter: ").lower()
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    print(display)
 
-print(display)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(
+            f"Current position: {position}\n Current Letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win! Game Over!")
