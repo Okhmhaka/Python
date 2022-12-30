6135ba961b7b0467670c8ea46c17b4b7f4addecc
Okhmhaka <github@conclavemail.com>
2022-12-30 09:09:46 -0500

    hangman-4

--- Python Class/Section 7/hangman4.py
+++ Python Class/Section 7/hangman4.py
@@ -68,7 +68,7 @@ word_length = len(chosen_word)
 lives = 6
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
-
+print(stages[lives])
 # Create blanks
 display = []
 for _ in range(word_length):
@@ -88,6 +88,12 @@ while not end_of_game:
     # Then reduce 'lives' by 1.
     # If lives goes down to 0 then the game should stop and it should print "You lose."
 
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Number of lives left: {lives}")
+        if lives == 0:
+            end_of_game = True
+            print("You Lose!")
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -97,3 +103,4 @@ while not end_of_game:
         print("You win.")
 
     # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
