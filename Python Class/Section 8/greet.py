1121d0525151d8707fb8559f637c1e88b58aa09a
Okhmhaka <github@conclavemail.com>
2023-07-09 22:55:33 -0400

    day 8

---
+++ Python Class/Section 8/greet.py
@@ -0,0 +1,27 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Good evening")
+#   print("Let's program in python")
+#   print("I want to learn data science. What about you?")
+  
+# greet()
+
+# def greet_with_name(name):
+#   print(f"Good evening {name}")
+#   print(f"{name}, let's program in python")
+#   print("I want to learn data science. What about you?")
+
+
+# greet_with_name("Brandon")
+
+# Functions with more than one input
+
+def greet_with(name, location):
+  print(f"Hello, my name is {name}.")
+  print(f"What is it like in {location}?") 
+
+greet_with(location="Tennessee" , name="Brandon")
