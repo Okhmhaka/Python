77874013f60cca2d8e1d8d1db7094df7e6366fc7
Okhmhaka <github@conclavemail.com>
2023-07-13 06:45:57 -0400

    dictionaries

---
+++ Python Class/Section 9/main.py
@@ -0,0 +1,20 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", 
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          
+                        }
+
+print(programming_dictionary) 
+
+#adding new tiems to dictionary
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+#create an empty dictionary
+empty_dictionary ={}
+
+# Wipe an existing dictionary
+
+#programming_dictionary = {}
+print(programming_dictionary)
